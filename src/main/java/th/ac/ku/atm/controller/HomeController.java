824e7135d1c52a3e7a72c63972b98be4e62ca306
aaomkk <Kuntarut.k@ku.th>
2021-01-07 01:47:10 +0700

    first commit

---
+++ src/main/java/th/ac/ku/atm/controller/HomeController.java
@@ -0,0 +1,17 @@
+package th.ac.ku.atm.controller;
+
+import jdk.jfr.ContentType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomepage(Model model) {
+        model.addAttribute("greeting","Sawaddee");
+        return "home";    // home template (home.html)
+
+    }
+}
